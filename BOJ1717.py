9a05f0aa262abb3f8ca06d5bfee780196ebcfbf7
tpgh02 <tpgh02@kookmin.ac.kr>
2023-03-27 18:35:56 +0900

    [BOJ] 1717 / gold5 / 20m

---
+++ BOJ1717.py
@@ -0,0 +1,34 @@
+n, m = map(int, input().split())
+mySet = list(range(n+1))
+
+def getParent(list, a):
+    if(list[a] == a):
+        return a
+    list[a] = getParent(list, list[a])
+    return list[a]
+
+def unionParent(list, a, b):
+    a = getParent(list, a)
+    b = getParent(list, b)
+    if(a<b): list[b] = a
+    else: list[a] = b     
+    return list
+    
+def isSame(list, a, b):
+    a = getParent(list, a)
+    b = getParent(list, b)
+    
+    if(a == b): print("YES")
+    else : print("NO")
+
+for i in range(m):
+    c, a, b = map(int, input().split())
+    
+    if(c == 0):
+        mySet = unionParent(mySet, a, b)
+    else :
+        if(getParent(list, a) == getParent(list, b)):
+            print("YES")
+        else :
+            print("NO")
+        
